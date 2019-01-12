5645c0c09266b03120c57a144d4ce34c20adee07
Douwe Osinga <douwe.osinga@gmail.com>
2019-01-12 17:51:01 -0500

    Internal state (#24)

--- simplegrid/deep_cow.py
+++ simplegrid/deep_cow.py
@@ -1,11 +1,10 @@
 from simplegrid.cow import SimpleCow, Action, MAX_ENERGY
 from simplegrid.dqn_agent import DQNAgent
 from shared.constants import VIEW_DISTANCE
-
+import numpy as np
 
 class DeepCow(SimpleCow):
-    state_size = (2 * VIEW_DISTANCE + 1) * (2 * VIEW_DISTANCE + 1)
-    agent = DQNAgent(state_size, action_size=4)
+    agent = None
 
     def __init__(self, x, y, energy, color=None):
         super().__init__(x, y, energy, color)
@@ -19,7 +18,26 @@ class DeepCow(SimpleCow):
         self.action_idx = DeepCow.agent.act(self.state)
         return Action(self.action_idx + 1)
 
+    def to_internal_state(self, state):
+        """Convert state to an internal representation.
+
+        The input state is a NxN matrix with for each cell either a 1 for food,
+        0 for nothing or -x for another animal. The center cell is always "us"
+        """
+
+        grass = np.copy(state).flatten()
+        grass[grass <= 0] = 0
+        cows = np.copy(state).flatten()
+        cows[cows >= 0] = 0
+        cows[cows < 0] = 1
+
+        return np.concatenate((grass, cows))
+
     def learn(self, state, reward, done):
+        state = self.to_internal_state(state)
+        if not DeepCow.agent:
+            DeepCow.agent = DQNAgent(len(state), action_size=4)
+
         if self.state is not None:
             DeepCow.agent.remember(self.state, self.action_idx, reward, state, done)
         self.state = state
